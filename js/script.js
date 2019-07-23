43b457e8b0f25309e3b206d0c7970d1fb0a773b6
Chinh Do <kieu-chinh.do@students.mq.edu.au>
2019-07-23 21:27:02 +1000

    added jquery badge generator

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     $portfolioSection = $('#portfolio');
     
     // Declare JS Variables /////////////////////////////
-    var traede = ["branding", "graphic", "prototype"];
     var myProjects = [
         {
             name: "traede",
@@ -19,20 +18,17 @@ $(document).ready(function () {
     ]
     
     // HTML output ///////////////////////////////////// 
-    
-    function addJobBadge(project) {
-        var $badgeContainer = $('<div class="badge-container">');       
-        $portfolioSection.append($badgeContainer);        
-        for (i = 0; i < project.job.length; i++) {            
-            $jobBadge = $('<span>');
+    // Look through each project in myProjects array and create individual job badges
+    for (i = 0; i < myProjects.length; i++) {       
+        var $badgeContainer = $('<div class="badge-container">');        
+        $('#' + myProjects[i].name).append($badgeContainer);
+        for (j = 0; j < myProjects[i].job.length; j++) {
+            var $jobBadge = $('<span>');
             $jobBadge
             .appendTo($badgeContainer)
-            .append(project.job[i])
-            .addClass('badge badge-' + project.job[i]);
-        }
+            .append(myProjects[i].job[j])
+            .addClass('badge badge-' + myProjects[i].job[j]);
+        }        
     }
     
-    for (i = 0; i < myProjects.length; i++) {
-        addJobBadge(myProjects[i]);
-    }
 });
