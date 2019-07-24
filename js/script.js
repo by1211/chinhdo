d12b57b4fb849ac3476c81d0633771ce1099539f
Chinh <contact@chinhdo.co>
2019-07-25 07:19:42 +1000

    added portfolio section

--- js/script.js
+++ js/script.js
@@ -1,33 +1,91 @@
-$('body').scrollspy({ target: '#navbar' })
-
 // jQuery methods go here...
 $(document).ready(function () {  
     // Declare CSS Variables ////////////////////////////
+    
     $portfolioSection = $('#portfolio');
     
     // Declare JS Variables /////////////////////////////
+    
     var myProjects = [
         {
-            name: "traede",
+            name: "Traede",
             job: ["branding", "graphic", "prototype"]
         },
         {
-            name: "skytr33",
+            name: "Skytr33",
             job: ["branding", "graphic", "webdesign"]
+        },
+        {
+            name: "FoodGiving",
+            job: ["branding", "webdesign", "prototype"]
+        },
+        {
+            name: "WanderSpace",
+            job: ["branding"]
+        },
+        {
+            name: "MechanicSMS",
+            job: ["branding", "prototype", "illustration"]
+        }
+    ]
+    
+    var otherProjects = [
+        {
+            name: "HeliLabs",
+            job: ["logo"]
+        },
+        {
+            name: "NeonSkies",
+            job: ["logo"]
+        },
+        {
+            name: "Rush",
+            job: ["logo"]
+        },
+        {
+            name: "Dr.Egg Adventure",
+            job: ["illustration"]
+        },
+        {
+            name: "BlanketNinja",
+            job: ["prototype"]
         }
     ]
     
     // HTML output ///////////////////////////////////// 
+    
+    // Materialize JS components
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+    
     // Look through each project in myProjects array and create individual job badges
-    for (i = 0; i < myProjects.length; i++) {       
-        var $badgeContainer = $('<div class="badge-container">');        
-        $('#' + myProjects[i].name).append($badgeContainer);
+    for (i = 0; i < myProjects.length; i++) {   
+        // Create banner
+        $projectBanner = $('<img>')
+        .attr({
+            alt: myProjects[i].name + "'s Logo Background",
+            src: 'images/backgrounds/' + myProjects[i].name + '.jpg',
+        });
+        $projectLogo = $('<img>')
+        .attr({
+            alt: myProjects[i].name + "'s Logo",
+            class: 'p-logo',
+            src: 'images/logos/' + myProjects[i].name + '.png',
+        });       
+        $('#' + myProjects[i].name)
+        .children('.p-banner') 
+        .append($projectLogo)   
+        .append($projectBanner); 
+        
         for (j = 0; j < myProjects[i].job.length; j++) {
-            var $jobBadge = $('<span>');
-            $jobBadge
-            .appendTo($badgeContainer)
+            // Create badge
+            $jobBadge = $('<div>')
             .append(myProjects[i].job[j])
-            .addClass('badge badge-' + myProjects[i].job[j]);
+            .addClass('chip chip-' + myProjects[i].job[j]);
+            // Add contents to its container
+            $('#' + myProjects[i].name)
+            .children('.chip-container')
+            .append($jobBadge);
         }        
     }
     
